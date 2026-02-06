c1f6d0f591b84c5973bfe42c87036ca744c399b5
oliver <omiguezalonso@danielcastelao.org>
2026-02-06 12:36:08 +0100

    Queda por ajustar muchas cosas

--- MultiplayerConfig/Lan/multiplayer_lan.gd
+++ MultiplayerConfig/Lan/multiplayer_lan.gd
@@ -5,10 +5,14 @@ extends Control
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene =preload("res://Scenes/MainCharacter/main_character.tscn")
 
+@onready var host: Button = $VBoxContainer/Host
+
 func _on_host_pressed() -> void:
 	peer.create_server(1025)
 	multiplayer.peer_connected.connect(_add_player)
 	_add_player()
+	host.disabled = true 
+	
 	
 func _add_player(id = 1):
 	var player = player_scene.instantiate()
